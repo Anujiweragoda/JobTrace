0826d74eb76686fd6b378e7dc9eec61a810fdec6
Anuji Nishala Weragoda <165540578+Anujiweragoda@users.noreply.github.com>
2026-09-08 12:56:06 +0530

    Update LoginPage.tsx

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -71,7 +71,7 @@ export default function LoginPage({ onLogin, onGoogleLogin, onSignup, loading = 
         window.google.accounts.id.renderButton(btnContainer, {
           theme: "outline",
           size: "large",
-          width: "458",
+          width: 450,
           text: "signin_with",
           locale: "en",
           shape: "rectangular",
